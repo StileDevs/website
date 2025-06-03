b10929c11cbe6a969263667649d01ece87822743
Hendra Gunawan <48410066+JadlionHD@users.noreply.github.com>
2025-06-03 15:49:32 +0700

    init files

---
+++ nuxt.config.ts
@@ -0,0 +1,14 @@
+import tailwindcss from "@tailwindcss/vite";
+
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: "2025-05-15",
+  devtools: { enabled: false },
+
+  modules: ["@nuxt/content", "@nuxt/eslint", "@nuxt/fonts", "@nuxt/icon", "@nuxt/image", "@nuxt/scripts", "@nuxt/test-utils"],
+
+  css: ["~/assets/css/main.css"],
+  vite: {
+    plugins: [tailwindcss()]
+  }
+});
---
+++ pages/index.vue
@@ -0,0 +1,5 @@
+<template>
+  <main>
+    <div class="text-red-500 font-bold">Hello world</div>
+  </main>
+</template>
